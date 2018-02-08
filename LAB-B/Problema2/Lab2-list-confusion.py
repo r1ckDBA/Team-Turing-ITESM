52b6b86a7d565431f697a50d236ce8600d169365
r1ckDBA <36052543+r1ckDBA@users.noreply.github.com>
2018-02-08 17:48:08 -0600

    Compara listas

--- LAB-B/Problema2/Lab2-list-confusion.py
+++ LAB-B/Problema2/Lab2-list-confusion.py
@@ -6,24 +6,29 @@
 #importa funcion random para generar cadena de 4 digitos
 from random import randint
 
+def comparaListas(listaA, ListaB):
+  listaC=[]
+
+  print ("ListaA:"+str(sorted(listaA)))
+  print ("ListaB:"+str(sorted(ListaB)))
+
+  for intA in listaA:
+    if intA in ListaB:
+      #Revisamos que el numero no este repetido
+      if intA not in listaC: 
+        listaC.append(int(intA))
+
+  return listaC
+
 #Define lista de numeros
 #Version normal
 #listaUno=[1, 1, 2, 3,33, 5, 8, 13, 21, 34, 55, 89,33]
 #listaDos=[1, 2, 3,33, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,33]
 
 #Version extra
-listaUno=list(range(0,randint(0,20),randint(1,9)))+list(range(0,randint(0,20),randint(1,9)))+list(range(0,randint(0,20),randint(1,9)))+list(range(0,randint(0,20),randint(1,9)))
+listaUno=list(range(0,randint(0,20),randint(1,9)))+list(range(1,randint(0,50),randint(1,9)))+list(range(2,randint(0,20),randint(1,9)))+list(range(0,randint(0,20),randint(1,9)))
 listaDos=list(range(0,randint(0,50),randint(1,9)))+list(range(0,randint(0,50),randint(1,9)))+list(range(0,randint(0,20),randint(1,9)))+list(range(0,randint(0,20),randint(1,9)))
 
-print ("ListaUno:"+str(sorted(listaUno)))
-print ("ListaDos:"+str(sorted(listaDos)))
-
-listaFinal=[]
-
-for intUno in listaUno:
-  if intUno in listaDos:
-    #Revisamos que el numero no este repetido
-    if intUno not in listaFinal: 
-        listaFinal.append(int(intUno))
+listaFinal=comparaListas(listaUno,listaDos)
 
 print("\n-----------\nLista final:"+str(sorted(listaFinal)))
