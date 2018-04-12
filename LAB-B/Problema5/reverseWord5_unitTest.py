30ed1f47c841b06b23c545421c9b5f67203632d3
Ubuntu <ubuntu@ip-172-31-37-158.us-east-2.compute.internal>
2018-04-12 04:42:03 +0000

    Fixed tab issues in unitTests

--- LAB-B/Problema5/reverseWord5_unitTest.py
+++ LAB-B/Problema5/reverseWord5_unitTest.py
@@ -2,12 +2,12 @@ import unittest
 from reverseWord5 import reverseWord
 
 class reverseWord_unittest(unittest.TestCase):
-	def test_case_example(self):
-		self.assertEqual( reverseWord( "My name is Michele" ), "Michele is name My ")
- 	def test_case_example_spaces(self):
- 		self.assertEqual( reverseWord( "Myname isMichele" ), "isMichele Myname ")
- 	def test_case_negative(self):
- 		self.assertNotEqual( reverseWord( "Myname isMichele" ), "isMichele Myname fds")
+    def test_case_example(self):
+        self.assertEqual( reverseWord( "My name is Michele" ), "Michele is name My ")
+    def test_case_example_spaces(self):
+        self.assertEqual( reverseWord( "Myname isMichele" ), "isMichele Myname ")
+    def test_case_negative(self):
+        self.assertNotEqual( reverseWord( "Myname isMichele" ), "isMichele Myname fds")
  
 if __name__ == '__main__':
     unittest.main()
--- LAB-B/Problema6/Lab6-cowsbulls-UNITEST.py
+++ LAB-B/Problema6/Lab6-cowsbulls-UNITEST.py
@@ -9,10 +9,10 @@ from Lab6cowsbulls import revisaVacas
 from Lab6cowsbulls import revisaToros
 
 class listAnalysis_unittest(unittest.TestCase):
-	def test_case_example(self):
-		self.assertEqual( revisaVacas([0,1,2,3],[5,1,5,3]),2 )
-	def test_case_example(self):
-		self.assertEqual( revisaToros([0,1,2,3],[3,3,3,5]),3 )
+    def test_case_example(self):
+        self.assertEqual( revisaVacas([0,1,2,3],[5,1,5,3]),2 )
+    def test_case_example(self):
+        self.assertEqual( revisaToros([0,1,2,3],[3,3,3,5]),3 )
  
 if __name__ == '__main__':
     unittest.main()
