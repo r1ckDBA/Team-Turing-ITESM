e7556108318ecc69385b1be71fcd4e697861805c
r1ckDBA <36052543+r1ckDBA@users.noreply.github.com>
2018-04-12 11:45:53 -0500

    Update Lab6-cowsbulls-UNITEST.py

--- LAB-B/Problema6/Lab6-cowsbulls-UNITEST.py
+++ LAB-B/Problema6/Lab6-cowsbulls-UNITEST.py
@@ -12,8 +12,7 @@ class listAnalysis_unittest(unittest.TestCase):
     def test_case_example(self):
         self.assertEqual( revisaVacas([0,1,2,3],[5,1,5,3]),2 )
     def test_case_example(self):
-        self.assertEqual( revisaToros([0,1,2,3],[3,3,3,5]),3 )
+        self.assertEqual( revisaToros([0,1,2,3],[7,7,7,0]),1 )
  
 if __name__ == '__main__':
     unittest.main()
-
