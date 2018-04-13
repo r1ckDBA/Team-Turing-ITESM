adddd674a97028be3c1c2b9479c07069e37732b2
jcvargas <juancarlos.itc@gmail.com>
2018-04-12 21:39:46 -0500

    Update runtest.sh
    added static analysis corrected

--- LAB-B/runtest.sh
+++ LAB-B/runtest.sh
@@ -26,5 +26,24 @@ echo "Evaluating oddOrEven.py"
 cd /var/lib/jenkins/workspace/Free\ Style\ Project/LAB-B/Probelma1/
 nohup pylint3 oddOrEven.py 2> /home/ubuntu/test_results/oddOrEven_static_analysis.log &
 
+echo "Evaluating Lab2listconfusion.py"
+cd  /var/lib/jenkins/workspace/Free\ Style\ Project/LAB-B/Problema2/
+nohup pylint3 Lab2listconfusion.py 2> /home/ubuntu/test_results/Lab2listconfusion_static_analysis.log &
+
+echo "Evaluating listAnalysis3.py"
+cd  /var/lib/jenkins/workspace/Free\ Style\ Project/LAB-B/Problema3/
+nohup pylint3 listAnalysis3.py 2> /home/ubuntu/test_results/listAnalysis3_static_analysis.log &
+
+echo "Evaluating fileOverlap.py"
+cd  /var/lib/jenkins/workspace/Free\ Style\ Project/LAB-B/Problema4/
+nohup pylint3 fileOverlap.py 2> /home/ubuntu/test_results/fileOverlap_static_analysis.log &
+
+echo "Evaluating reverseWord5.py"
+cd  /var/lib/jenkins/workspace/Free\ Style\ Project/LAB-B/Problema5/
+nohup pylint3 reverseWord5.py 2> /home/ubuntu/test_results/reverseWord5_static_analysis.log &
+
+echo "Evaluating Lab6cowsbulls.py"
+cd  /var/lib/jenkins/workspace/Free\ Style\ Project/LAB-B/Problema6/
+nohup pylint3 Lab6cowsbulls.py 2> /home/ubuntu/test_results/Lab6cowsbulls_static_analysis.log &
 
 echo "Done "
